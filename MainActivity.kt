ae6f857f03380574998735a993d092a1514d486b
Shiva-Surendran <107458185+Shiva-Surendran@users.noreply.github.com>
2024-03-29 22:21:15 +0530

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,18 +18,15 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.first.ui.theme.FirstTheme
 import androidx.navigation.compose.rememberNavController
 
 
@@ -43,15 +41,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun details() {
+fun details(navController: NavController) {
     Column(modifier = Modifier.padding(10.dp)) {
         Button(
-            onClick = {},
+            onClick = {
+                      navController.navigate("form")
+
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 8.dp)
                 .height(56.dp)
                 .padding(horizontal = 16.dp)
+                .clickable {  navController.navigate("form")
+                }
         ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
@@ -75,8 +78,8 @@ fun details() {
 @Composable
 fun app() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "details") {
-        composable(route = "details") { details() }
+    NavHost(navController = navController, startDestination = "detail") {
+        composable(route = "detail") { details(navController) } // Pass navController
         composable(route = "form") { FormScreen() }
     }
 }
